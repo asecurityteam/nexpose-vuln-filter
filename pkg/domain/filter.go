97ad5cb4311b9cb25cef7f0cff93ec18ba49059a
Austin Burnett <aburnett@atlassian.com>
2019-03-27 14:39:08 -0500

    [SECD-367] Drop unused type

--- pkg/domain/filter.go
+++ pkg/domain/filter.go
@@ -1,21 +1,10 @@
 package domain
 
 import (
-	"fmt"
-
 	"github.com/asecurityteam/nexpose-vuln-filter/pkg/domain/nexpose"
 )
 
 // VulnFilterer is the expected form of filters applied to Nexpose vulnerabilities.
 type VulnFilterer interface {
 	FilterVulnerabilities([]nexpose.AssetVulnerabilityDetails) []nexpose.AssetVulnerabilityDetails
-}
-
-// ErrInvalidInput indicates that the Vulnerability did not have the expected shape.
-type ErrInvalidInput struct {
-	Reason string
-}
-
-func (e ErrInvalidInput) Error() string {
-	return fmt.Sprintf("invalid input: %s", e.Reason)
 }
