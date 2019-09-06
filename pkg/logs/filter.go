556250d86e1c9ce230fd989142ad8b26ef930cb3
Ben Krebsbach <bkrebsbach@atlassian.com>
2019-09-06 10:28:05 -0500

    Replace VulnRetained action in logs with VulnAccepted to make logs and stats consistent with each other

--- pkg/logs/filter.go
+++ pkg/logs/filter.go
@@ -1,17 +1,17 @@
 package logs
 
 const (
-	// VulnRetained is an Action used to signify the vuln was retained
-	VulnRetained = "retained"
-	// VulnDiscarded is an Action used to signify the vuln was discarded
+	// VulnAccepted is an Action used to signify the vuln was accepted.
+	VulnAccepted = "accepted"
+	// VulnDiscarded is an Action used to signify the vuln was discarded.
 	VulnDiscarded = "discarded"
-	// CvssV2Score signifies the method used to retain the vulnerability was the CVSS V2 Score
+	// CvssV2Score signifies the method used to retain the vulnerability was the CVSS V2 Score.
 	CvssV2Score = "cvss_v2_score"
-	// VulnID signifies the method used to retain the vulnerability was the vuln id
+	// VulnID signifies the method used to retain the vulnerability was the vuln id.
 	VulnID = "vuln_id"
 )
 
-// VulnerabilityFiltered contains details on the filtering of an asset's vulnerabilities
+// VulnerabilityFiltered contains details on the filtering of an asset's vulnerabilities.
 type VulnerabilityFiltered struct {
 	Message string `logevent:"message,default=vulnerability-filtered"`
 	Action  string `logevent:"action"`
